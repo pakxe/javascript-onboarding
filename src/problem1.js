292fae92b392496a5a3d57c4ac1eeb5bd557e544
pakxe <pigkill40@naver.com>
2022-11-02 03:19:02 +0900

    feat: [P1] 다시 풀어보기

--- src/problem1.js
+++ src/problem1.js
@@ -1,50 +1,45 @@
-const LAST_PAGE = 400;
+// 1:12 -> 1:30
 
 function problem1(pobi, crong) {
-  if (!isValidPage(pobi) || !isValidPage(crong)) return -1;
-
+  if (!isValidPages(pobi) || !isValidPages(crong)) return -1; //하나라도 false면 걸리게..
   const maxPobi = maxNum(pobi);
   const maxCrong = maxNum(crong);
+
   if (maxPobi > maxCrong) return 1;
   if (maxPobi < maxCrong) return 2;
   if (maxPobi === maxCrong) return 0;
 }
 
-//페이지 유효성 검사
-function isValidPage(book) {
-  if (book.length !== 2) return false;
-  if (book[0] < 1 || book[1] > LAST_PAGE) return false;
-  if (book[1] - book[0] !== 1) return false;
-  if (book[1] % 2 !== 0) return false;
+const maxNum = (pages) => {
+  const calculatedPages = pages.reduce((numArr, page) => {
+    numArr.push(sumNum(page));
+    numArr.push(mulNum(page));
+    return numArr;
+  }, []);
+
+  return Math.max(...calculatedPages);
+};
+
+const sumNum = (num) => {
+  const numArr = changeToNumArr(num);
+  return numArr.reduce((sum, number) => sum + number, 0);
+};
+
+const mulNum = (num) => {
+  const numArr = changeToNumArr(num);
+  return numArr.reduce((mul, number) => mul * number, 1);
+};
+
+const isValidPages = (pages) => {
+  const [leftPage, rightPage] = pages;
+  if (pages.length !== 2) return false;
+  if (leftPage < 1 || rightPage > 400) return false;
+  if (rightPage - leftPage !== 1) return false;
+  if (rightPage % 2 !== 0) return false;
   else return true;
-}
+};
 
-// 2, 3번의 최대 값을 반환하는 함수
-function maxNum(book) {
-  const numList = [];
-
-  book.forEach((page) => {
-    numList.push(sum(page));
-    numList.push(mul(page));
-  });
-
-  return Math.max(...numList);
-}
-
-// 각 자리수를 더해서 반환하는 함수
-function sum(num) {
-  const numToString = num.toString().split("");
-  const sum = numToString.reduce((m, n) => m + Number(n), 0);
-
-  return sum;
-}
-
-// 각 자리수를 곱해서 반환하는 함수
-function mul(num) {
-  const numToString = num.toString().split("");
-  const mul = numToString.reduce((m, n) => m * Number(n), 1);
-
-  return mul;
-}
-
+const changeToNumArr = (num) => num.toString().split("").map(Number);
 module.exports = problem1;
+
+// reduce 메서드를 잘 활용할 것, 다중 메서드가 동일한 곳에서 반복된다면 따로 뗴어내서 함수로 만들자
