8e2bca5b0e167cd459b5dba62758f71ccc587748
pakxe <pigkill40@naver.com>
2023-09-15 16:08:39 +0900

    feat: 페이지 유효성 검사 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,23 +1,32 @@
+const [START_PAGE, LAST_PAGE] = [1, 400];
+
 const [POBI_WIN, CRONG_WIN] = [1, 2];
 const DRAW = 0;
 const EXCEPTION = -1;
 
 function problem1(pobi, crong) {
-	return answer;
+	// 유효한 페이지가 아닌 경우 게임 종료
+	if (!isValidPages(pobi) && !isValidPages(crong)) return EXCEPTION;
 }
-
+// 가독성이 안좋을 까봐 더이상 축약하지 않음.
 const totalSum = (pageNum) => {
-	const strPageNum = pageNum.toString().split('').map(Number);
+	const strPageNum = getNumToArr(pageNum);
 
 	return strPageNum.reduce((sum, num) => sum + num, 0);
 };
 
 const totalMul = (pageNum) => {
-	const strPageNum = pageNum.toString().split('').map(Number);
+	const strPageNum = getNumToArr(pageNum);
 
 	return strPageNum.reduce((sum, num) => sum * num, 1);
 };
 
 const getNumToArr = (num) => num.toString().split('').map(Number);
+
+const isValidPages = ([left, right]) => {
+	if (left < START_PAGE) return false;
+	if (right > LAST_PAGE) return false;
+	return true;
+};
 
 module.exports = problem1;
