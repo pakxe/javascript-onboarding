426b8488e25e1881d1d4cb40138fa7a3a176a9a6
pakxe <pigkill40@naver.com>
2023-09-15 16:25:13 +0900

    feat: 각자의 합과 곱을 계산해 최댓값을 구하는 로직 작성

--- src/problem1.js
+++ src/problem1.js
@@ -7,7 +7,22 @@ const EXCEPTION = -1;
 function problem1(pobi, crong) {
 	// 유효한 페이지가 아닌 경우 게임 종료
 	if (!isValidPages(pobi) && !isValidPages(crong)) return EXCEPTION;
+
+	// const [pobiLeft, pobiRight] = pobi;
+	// const [crongLeft, crongRight] = crong;
+
+	const pobiMax = getMax(pobi);
+	const crongMax = getMax(crong);
 }
+
+const getMax = (pages) => {
+	const pagesMax = pages.map(([left, right]) =>
+		Math.max(totalSum(left), totalMul(right))
+	);
+
+	return Math.max(...pagesMax);
+};
+
 // 가독성이 안좋을 까봐 더이상 축약하지 않음.
 const totalSum = (pageNum) => {
 	const strPageNum = getNumToArr(pageNum);
